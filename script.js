d20fc73da968efe3e4b522b4a8adb5b947b07f82
Lynn Chaker <lchaker921@tamu.edu>
2026-04-14 17:47:57 +0000

    Cat picture feature implemented

--- script.js
+++ script.js
@@ -21,4 +21,27 @@ function fetchCatFact() {
     });
 }
 
+function fetchCatPicture() {
+  output.textContent = "Loading cat picture...";
+  
+  fetch("https://api.thecatapi.com/v1/images/search")
+    .then(function (response) {
+      if (!response.ok) {
+        throw new Error("Network response was not OK");
+      }
+      return response.json();
+    })
+    .then(function (data) {
+      const img = document.createElement("img");
+      img.src = data[0].url;
+      img.alt = "A cute cat";
+      output.innerHTML = "";
+      output.appendChild(img);
+    })
+    .catch(function (error) {
+      output.textContent = "Something went wrong. Please try again.";
+      console.error("Fetch error:", error);
+    });
+}
+
 factButton.addEventListener("click", fetchCatFact);
